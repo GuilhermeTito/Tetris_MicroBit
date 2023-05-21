6f69ec6dfb81a83b638198b22f799aaa73edd262
Guilherme Ferraz Ronha <112720645+GuilhermeTito@users.noreply.github.com>
2023-05-21 18:18:21 -0300

    Criei os tetraminós

--- main.ts
+++ main.ts
@@ -1,26 +1,70 @@
-let x = 0
-let y = 0
-let ponto = {0 : game.createSprite(x, y + 1),
-             1 : game.createSprite(x + 1, y + 1),
-             2 : game.createSprite(x + 1, y) }
-let t = 0
-basic.forever(function(){
-    if(t % 10 == 0){
-        ponto[0].setX(x)
-        ponto[0].setY(y + 1)
-        ponto[1].setX(x + 1)
-        ponto[1].setY(y + 1)
-        ponto[2].setX(x + 1)
-        ponto[2].setY(y)
-        y++
-        if (y > 3) {
-            y = 0
-            x++
-        }
-        if(x > 3)
-        {
-            x = 0
-        }
+class tetramino_meioI {
+    x: number
+    y: number
+    p0: game.LedSprite
+    p1: game.LedSprite
+
+    constructor(x: number, y: number) {
+        this.x = x
+        this.y = y
+        this.p0 = game.createSprite(this.x, this.y)
+        this.p1 = game.createSprite(this.x, this.y + 1)
     }
-    t++
-})
+}
+
+class tetramino_O
+{
+    x : number
+    y : number
+    p00: game.LedSprite
+    p01: game.LedSprite
+    p10: game.LedSprite
+    p11: game.LedSprite
+    
+    constructor(x : number, y : number)
+    {
+        this.x = x
+        this.y = y
+        this.p00 = game.createSprite(this.x, this.y)
+        this.p01 = game.createSprite(this.x + 1, this.y)
+        this.p10 = game.createSprite(this.x, this.y + 1)
+        this.p11 = game.createSprite(this.x + 1, this.y + 1)
+    }
+}
+
+class tetramino_curtoL {
+    x: number
+    y: number
+    p00: game.LedSprite
+    p01: game.LedSprite
+    p10: game.LedSprite
+
+    constructor(x: number, y: number) {
+        this.x = x
+        this.y = y
+        this.p00 = game.createSprite(this.x, this.y)
+        this.p01 = game.createSprite(this.x + 1, this.y)
+        this.p10 = game.createSprite(this.x, this.y + 1)
+    }
+}
+
+class tetramino_curtoJ {
+    x: number
+    y: number
+    p00: game.LedSprite
+    p01: game.LedSprite
+    p11: game.LedSprite
+
+    constructor(x: number, y: number) {
+        this.x = x
+        this.y = y
+        this.p00 = game.createSprite(this.x, this.y)
+        this.p01 = game.createSprite(this.x + 1, this.y)
+        this.p11 = game.createSprite(this.x + 1, this.y + 1)
+    }
+}
+
+let i = new tetramino_meioI(0, 0)
+let o = new tetramino_O(3, 0)
+let l = new tetramino_curtoL(0 , 3)
+let j = new tetramino_curtoJ(3, 3)
